4afe3dec40f7409839c26e888bdac75c690e3890
Alejandro Osorio <alejandro.osorio@walmart.com>
2024-02-05 15:33:35 -0500

    layout components: create different list types

--- src/RegularList.js
+++ src/RegularList.js
@@ -5,9 +5,9 @@ export const RegularList = ({
 }) => {
 	return (
 		<>
-		{items.map((item, i) => (
-			<ItemComponent key={i} {...{ [resourceName]: item }} />
-		))}
+			{items.map((item, i) => (
+				<ItemComponent key={i} {...{ [resourceName]: item }} />
+			))}
 		</>
-	)
-}
+	);
+};
