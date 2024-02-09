454f7f3b44b058661106af3db3ccc875638ad9da
Alejandro Osorio <alejandro.osorio@walmart.com>
2024-02-09 12:54:02 -0500

    Controlled vs uncontrolled components: Creating a controlled onboarding flow

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
+import { useState } from 'react';
+import { ControlledOnboardingFlow } from './ControlledOnboardingFlow';
 
 const StepOne = ({ goToNext }) => (
 	<>
-	<h1>Step 1</h1>
-	<button onClick={() => goToNext({ name: 'John Doe' })}>Next</button>
+		<h1>Step 1</h1>
+		<button onClick={() => goToNext({ name: 'John Doe' })}>Next</button>
 	</>
 );
 const StepTwo = ({ goToNext }) => (
 	<>
-	<h1>Step 2</h1>
-	<button onClick={() => goToNext({ age: 100 })}>Next</button>
+		<h1>Step 2</h1>
+		<button onClick={() => goToNext({ age: 100 })}>Next</button>
 	</>
 );
 const StepThree = ({ goToNext }) => (
 	<>
-	<h1>Step 3</h1>
-	<button onClick={() => goToNext({ hairColor: 'brown' })}>Next</button>
+		<h1>Step 3</h1>
+		<p>You qualify for senior discount</p>
+		<button onClick={() => goToNext({})}>Next</button>
+	</>
+);
+const StepFour = ({ goToNext }) => (
+	<>
+		<h1>Step 4</h1>
+		<button onClick={() => goToNext({ hairColor: 'brown' })}>Next</button>
 	</>
 );
 
 function App() {
+	const [onboardingData, setOnboardingData] = useState({});
+	const [currentIndex, setCurrentIndex] = useState(0);
+
+	const onNext = stepData => {
+		setOnboardingData({ ...onboardingData, ...stepData });
+		setCurrentIndex(currentIndex + 1);
+	};
+
 	return (
-		<UncontrolledOnboardingFlow onFinish={data => {
-			console.log(data);
-			alert('Onboarding complete!');
-		}}>
+		<ControlledOnboardingFlow
+			currentIndex={currentIndex}
+			onNext={onNext}
+		>
 			<StepOne />
 			<StepTwo />
-			<StepThree />
-		</UncontrolledOnboardingFlow>
+			{onboardingData.age >= 62 && <StepThree />}
+			<StepFour />
+		</ControlledOnboardingFlow>
 	);
 }
 
